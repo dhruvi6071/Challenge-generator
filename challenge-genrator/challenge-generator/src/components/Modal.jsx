260a695585218f2294845557a996001e66efa901
dhruvi6071 <dhruvi6071@gmail.com>
2024-08-26 00:46:46 +0530

    use-AnimatePresence-and-exit-to-make-sure-there-is-an-animation-in-exit-too

--- challenge-genrator/challenge-generator/src/components/Modal.jsx
+++ challenge-genrator/challenge-generator/src/components/Modal.jsx
@@ -8,6 +8,7 @@ export default function Modal({ title, children, onClose }) {
       <motion.dialog 
       initial={{opacity: 0, y: 30}} //It is use to set animation without any props or condition.
       animate={{ opacity: 1, y:0}} 
+      exit={{opacity:0, y:30}} // It is used to set disappearance animation.
       open className="modal">
         <h2>{title}</h2>
         {children}
